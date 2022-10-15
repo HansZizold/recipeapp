67efdcecc8fef315f9caead0ad679bf2fc758d5c
officialesse <uzomaesse@gmail.com>
2022-10-16 00:04:12 +0100

    @HeDevedUp feat: bug removed

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -15,34 +15,35 @@ class FoodsController < ApplicationController
       redirect_to foodlists_path
     else
       flash.now[:error] = 'Error: foodList could not be deleted'
-    @foods = Food.all
-  end
-
-  def new
-    @food = Food.new
-  end
-
-  def create
-    @food = Food.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
-    @food.user = current_user
-
-    if @food.save
-      flash[:success] = 'Ingredient saved successfully'
-      redirect_to foods_path(current_user)
-    else
-      flash.now[:error] = 'Error: Ingredient could not be saved'
+      @foods = Food.all
     end
-  end
 
-  def destroy
-    food = Food.find(params[:id])
-    food.destroy
+    def new
+      @food = Food.new
+    end
 
-    if food.destroy
-      flash[:success] = 'Food deleted successfully'
-      redirect_to foods_path
-    else
-      flash.now[:error] = 'Error: Recipe could not be deleted'
+    def create
+      @food = Food.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
+      @food.user = current_user
+
+      if @food.save
+        flash[:success] = 'Ingredient saved successfully'
+        redirect_to foods_path(current_user)
+      else
+        flash.now[:error] = 'Error: Ingredient could not be saved'
+      end
+    end
+
+    def destroy
+      food = Food.find(params[:id])
+      food.destroy
+
+      if food.destroy
+        flash[:success] = 'Food deleted successfully'
+        redirect_to foods_path
+      else
+        flash.now[:error] = 'Error: Recipe could not be deleted'
+      end
     end
   end
 end
